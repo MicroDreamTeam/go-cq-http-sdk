857f391a5e9f246ac9dea1d696255b4dbda2a2ad
虞灪 <995645888@qq.com>
2023-06-01 16:31:49 +0800

    feat: 支持getStrangerInfo Api

--- src/Apis/Friend.php
+++ src/Apis/Friend.php
@@ -4,10 +4,32 @@ namespace Itwmw\GoCqHttp\Apis;
 
 use Itwmw\GoCqHttp\Data\ArrayData;
 use Itwmw\GoCqHttp\Data\GetFriendList;
+use Itwmw\GoCqHttp\Data\GetStrangerInfo;
 use Itwmw\GoCqHttp\Data\GetUnidirectionalFriendList;
 
 class Friend extends BaseApi
 {
+    /**
+     * 获取陌生人信息
+     *
+     * @param int  $user_id  QQ 号
+     * @param bool $no_cache 是否不使用缓存（使用缓存可能更新不及时, 但响应更快）
+     *
+     * @return GetStrangerInfo
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Itwmw\GoCqHttp\Exceptions\ApiException
+     * @throws \Itwmw\GoCqHttp\Exceptions\ApiHttpException
+     *
+     * @noinspection PhpFullyQualifiedNameUsageInspection
+     */
+    public function getStrangerInfo(int $user_id, bool $no_cache = false): GetStrangerInfo
+    {
+        return $this->client->post('/get_stranger_info', [
+            'form_params' => compact('user_id', 'no_cache')
+        ])->getData();
+    }
+
     /**
      * 删除好友
      *
---
+++ src/Data/GetStrangerInfo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Itwmw\GoCqHttp\Data;
+
+use JetBrains\PhpStorm\ExpectedValues;
+
+class GetStrangerInfo extends CreateData
+{
+    /**
+     * @param int    $user_id    QQ 号
+     * @param string $nickname   昵称
+     * @param string $sex        性别, male 或 female 或 unknown
+     * @param int    $age        年龄
+     * @param string $qid        qid ID身份卡
+     * @param int    $level      等级
+     * @param int    $login_days 登录天数
+     */
+    public function __construct(
+        public readonly int $user_id,
+        public readonly string $nickname,
+        #[ExpectedValues(values: ['male', 'female', 'unknown'])]
+        public readonly string $sex,
+        public readonly int $age,
+        public readonly string $qid,
+        public readonly int $level,
+        public readonly int $login_days,
+    ) {
+    }
+}
